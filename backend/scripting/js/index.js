35ce476d02d679cc41fe5e7fac8ff7b87ce3cbde
Kecvin-12-u <65631970+Kevin12-0@users.noreply.github.com>
2022-12-20 11:19:00 -0600

    cases

--- backend/scripting/js/index.js
+++ backend/scripting/js/index.js
@@ -1,6 +1,17 @@
 parrafoIncial = document.getElementById("parrafoInical");
 
 /* Cases
-PascalCase
+PascalCase => Clases -JAVA, Python
 
+camelCase => variables, funciones, atributos. - Java - JavaScript
+    variable => parrafoIncial = 
+    funcion => parrafoIncial()
+    atributo => parrafoIncial
+snacke_case = Python, Js
+    variables y funciones 
+CASE => Constantes 
 */
+
+parrafoIncial.onclick = function(){
+    accion()
+}
