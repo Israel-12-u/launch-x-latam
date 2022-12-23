e0baf611d044a79a3290014e7f231f87919664d1
Kecvin-12-u <65631970+Kevin12-0@users.noreply.github.com>
2022-12-22 23:57:09 -0600

    calculadora complera

--- backend/calculadora/calculadora.js
+++ backend/calculadora/calculadora.js
@@ -37,3 +37,42 @@ function divicion() {
     let result = number1 / number2;
     resultadoEtiqueta.innerHTML = result;
 }
+
+function potencia() {
+    let number1 = document.getElementById("numero1").value;
+    let number2 = document.getElementById("numero2").value;
+    let resultadoEtiqueta = document.getElementById("result");
+    number1 = parseFloat(number1);
+    number2 = parseFloat(number2);
+    let result = Math.pow(number1,number2); ;
+    resultadoEtiqueta.innerHTML = result;
+}
+
+function raizCuadrada() {
+    let number1 = document.getElementById("numero1").value;
+    let number2 = document.getElementById("numero2").value;
+    let resultadoEtiqueta = document.getElementById("result");
+    number1 = parseFloat(number1);
+    number2 = parseFloat(number2);
+    let result = Math.pow(number1,1/number2); ;
+    resultadoEtiqueta.innerHTML = result;
+}
+
+function residuo() {
+    let number1 = document.getElementById("numero1").value;
+    let number2 = document.getElementById("numero2").value;
+    let resultadoEtiqueta = document.getElementById("result");
+    number1 = parseFloat(number1);
+    number2 = parseFloat(number2);
+    let result = number1 % number2;
+    resultadoEtiqueta.innerHTML = result;
+}
+
+function reset() {
+    let number1 = document.getElementById("numero1");
+    let number2 = document.getElementById("numero2");
+    number1.value = 0;
+    number2.value = 0;
+    let resultadoEtiqueta = document.getElementById("result");
+    resultadoEtiqueta.innerHTML = 0;
+}
