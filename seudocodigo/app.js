5a483eef719aa5259c278caa6de5255032564046
Kecvin-12-u <65631970+Kevin12-0@users.noreply.github.com>
2023-03-29 11:54:34 -0600

    fin de examen de ids parte algoritmos

--- seudocodigo/app.js
+++ seudocodigo/app.js
@@ -46,7 +46,76 @@ function arregloOrdenado() {
     }
     return array3;
 }
-
 console.log(arregloOrdenado(array3));
  */
 
+
+/* console.log("---Numero repetidos en un array---");
+
+const array1 = [1, 3, 3, 4, 5, 6, 8, 9, 9]
+const array2 = [2, 2, 3, 5, 7, 9, 9]
+function numerosRepetidos() {
+    let array3 = [];
+    for (let i = 0; i < array1.length; i++) {
+        for (let j = 0; j < array2.length; j++) {
+            if (array1[i] === array2[j]) {
+                array3.push(array1[i]);
+                break;
+            }
+        }
+    }
+    return (array3);
+}
+console.log(numerosRepetidos(array1,array2)); */
+
+/* const a = 5;
+const b = 3;
+const c = 8;
+function validarMedias() {
+    if (a + b > c && a + c > b && b + c > a) {
+        console.log("Se puede construir el triangulo");
+    } else {
+        console.log("no se puede construir el triangulo");
+    }
+}
+const medidasLados = validarMedias(a,b,c);
+console.log(medidasLados);
+ */
+
+/* const array1 = [1, 5, 9, 2, 15, 9, 10, 12];
+function valoresMaximos() {
+    let i = 0;
+    let numero1 = 0;
+    let numero2 = 0;
+    while(i < array1.length) {
+        if(array1[i] > numero1) {
+            numero2 = numero1;
+            numero1 = array1[i];
+        } else if(array1[i] > numero2 && array1[i] !== numero1){
+            numero2 = array1[i]
+        }
+        i++;
+    }
+    return [numero1,numero2];
+}
+const resultado = valoresMaximos(array1);
+console.log(resultado); */
+
+/* let array1 = [3,4,1]
+function arregloOrdenado() {
+    let i = 0;
+    while (i < array1.length) {
+        let j = i + 1;
+        while (j < array1.length) {
+            if (array1[i] > array1[j]) {
+                let temp = array1[i];
+                array1[i] = array1[j];
+                array1[j] = temp;
+            }
+            j++;
+        }
+        i++;
+    }
+    return array1;
+}
+console.log(arregloOrdenado(array1)); */
